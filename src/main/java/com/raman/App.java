74073673e6e623005fa398c59b154679b5f4f22c
sung <you@example.com>
2022-02-02 09:05:57 +0000

    changetext 2

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Changes are done by dev-team" );
+        System.out.println( "Changes are done by dev-tea 222m" );
     }
 }
